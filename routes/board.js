4d59cc4bb9ae0bab08f6983c1a8f88d1e5dec061
qlqlrh <bluebird.happier@gmail.com>
2024-02-22 19:05:58 +0900

    게시글 작성 기능 구현

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.render("board", { title: "게시글 등록" });
+});
+
+router.post("/", function (req, res, next) {
+  const { title, content } = req.body;
+  console.log("title: ", title);
+  console.log("content: ", content);
 });
 
 module.exports = router;
